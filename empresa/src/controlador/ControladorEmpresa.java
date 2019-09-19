35db36e4f78e59c38540ec8196f55015a380527f
Administrador <Administrador@10.10.4.21>
2019-09-19 16:03:11 -0300

    cu

---
+++ empresa/src/controlador/ControladorEmpresa.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import dao.DaoEmpresa;
+import javax.swing.JOptionPane;
+import modelo.Empresa;
+import tela.manutencao.ManutencaoEmpresa;
+import java.util.List;
+
+import java.util.Vector;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+/**
+ *
+ * @author Administrador
+ */
+public class ControladorEmpresa {
+
+  public static void inserir(ManutencaoEmpresa man){
+        Empresa objeto = new Empresa();
+        objeto.setNome_fantasia(man.jtfnomefantasia.getText());
+        objeto.setRazao_social(man.jtfrazaosocial.getText());
+        
+        boolean resultado = DaoEmpresa.inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+}
+
+ public static void alterar(ManutencaoEmpresa man){
+        Empresa objeto = new Empresa();
+        //definir todos os atributos
+        objeto.setCodigo(Integer.parseInt(man.jtfcodigo.getText()));
+        objeto.setNome_fantasia(man.jtfnomefantasia.getText());
+        objeto.setRazao_social(man.jtfrazaosocial.getText());
+        
+        boolean resultado = DaoEmpresa.alterar(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+
+    
+        public static void excluir(ManutencaoEmpresa man){
+        Empresa objeto = new Empresa();
+        objeto.setCodigo(Integer.parseInt(man.jtfcodigo.getText())); //só precisa definir a chave primeira
+        
+        boolean resultado = DaoEmpresa.excluir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Excluído com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+        public static void atualizarTabela(JTable tabela) {
+        DefaultTableModel modelo = new DefaultTableModel();
+        //definindo o cabeçalho da tabela
+        modelo.addColumn("Código");
+        modelo.addColumn("Nome Fantasia");
+        modelo.addColumn("Razão Social");
+        List<Empresa> resultados = DaoEmpresa.consultar();
+        for (Empresa objeto : resultados) {
+            Vector linha = new Vector();
+            
+            //definindo o conteúdo da tabela
+            linha.add(objeto.getCodigo());
+            linha.add(objeto.getNome_fantasia());
+            linha.add(objeto.getRazao_social());
+            modelo.addRow(linha); //adicionando a linha na tabela
+        }
+        tabela.setModel(modelo);
+    }
+    
+}
